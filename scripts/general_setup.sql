a07068d5763b30012bb3355ed94d1b56218eb99f
BillRmz <bilyalon@gmail.com>
2025-03-07 22:53:20 -0600

    added files to general setup and ddl for logging table

--- scripts/general_setup.sql
+++ scripts/general_setup.sql
@@ -1,1 +1,21 @@
 CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA ADMIN;
+
+CREATE TABLE
+    ADMIN.ADMIN_LOGGING_DATA (
+        insert_id INT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+        table_name NVARCHAR (50),
+        from_stage NVARCHAR (50),
+        to_stage NVARCHAR (50),
+        rows_moved NVARCHAR (50),
+        source NVARCHAR (50),
+        insert_ts DATETIME
+    );
+
+SELECT
+    *
+FROM
+    ADMIN.ADMIN_LOGGING_DATA
