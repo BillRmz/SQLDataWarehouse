1f0e25f53f54fa920b7e74be8a47c405e2335d84
BillRmz <bilyalon@gmail.com>
2025-03-10 10:30:38 -0600

    Added SQL ext to a file, create logic to dinamyc load

--- scripts/general_setup.sql
+++ scripts/general_setup.sql
@@ -8,15 +8,44 @@ GO
 CREATE TABLE
     ADMIN.ADMIN_LOGGING_DATA (
         insert_id INT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
-        table_name NVARCHAR (50),
-        from_stage NVARCHAR (50),
-        to_stage NVARCHAR (50),
-        rows_moved NVARCHAR (50),
         source NVARCHAR (50),
+        table_name NVARCHAR (50),
+        stage NVARCHAR (50),
+        rows_moved NVARCHAR (50),
+        start_time DATETIME,
+        end_time DATETIME,
+        execution_time DATETIME,
         insert_ts DATETIME
     );
 
 SELECT
     *
 FROM
-    ADMIN.ADMIN_LOGGING_DATA
+    ADMIN.ADMIN_LOGGING_DATA GO
+CREATE TABLE
+    ADMIN.ADMIN_CONTROL_LOAD (
+        row_id INT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+        table_name VARCHAR(50),
+        source VARCHAR(50),
+        insert_ts DATETIME
+    );
+
+INSERT INTO
+    ADMIN.ADMIN_CONTROL_LOAD (table_name, source, insert_ts)
+VALUES
+    ('cust_info.csv', 'source_crm', GETDATE ())
+INSERT INTO
+    ADMIN.ADMIN_CONTROL_LOAD (table_name, source, insert_ts)
+VALUES
+    ('prd_info.csv', 'source_crm', GETDATE ()),
+    ('sales_details.csv', 'source_crm', GETDATE ())
+INSERT INTO
+    ADMIN.ADMIN_CONTROL_LOAD (table_name, source, insert_ts)
+VALUES
+    ('CUST_AZ12.csv', 'source_erp', GETDATE ()),
+    ('LOC_A101.csv', 'source_erp', GETDATE ()),
+    ('PX_CAT_G1V2.csv', 'source_erp', GETDATE ())
+SELECT
+    *
+FROM
+    ADMIN.ADMIN_CONTROL_LOAD;
